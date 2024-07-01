2e7a500a606b8f5cea6158e402606df3c6a6f0cb
JacobT <jacobfung04@gmail.com>
2024-07-01 14:30:57 +0800

    final project

--- sb-final-project/src/main/java/demo/com/sb_final_project/config/AppScheduler.java
+++ sb-final-project/src/main/java/demo/com/sb_final_project/config/AppScheduler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import demo.com.sb_final_project.entity.StockListEntity;
 import demo.com.sb_final_project.infra.RedisHelper;
 import demo.com.sb_final_project.service.StockListService;
@@ -34,6 +35,16 @@ public class AppScheduler {
   @Scheduled (cron = "0 55 8 * * *")
   public void systemDateRedis(){
     redisHelper.delete("stockSystemDate");
+  }
+
+  @Scheduled (cron = "0 56 8 * * *")
+  public void delStockListRedis(){
+    redisHelper.delete("stock-list");
+    try{
+      redisHelper.set("stock-list", stockListService.getStockList());
+    } catch(JsonProcessingException e){
+      throw new RuntimeException("Error:Fail to parse Json");
+    }
   }
 
 
